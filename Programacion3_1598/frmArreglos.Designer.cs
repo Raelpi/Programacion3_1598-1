c520b51ec145c4b7d8623f936c67a22326419878
Raelpi <rpinedab@unah.hn>
2021-06-24 02:23:47 -0600

    primer comit
    este es el primer commit de la clase de programacion 3 de la seccion 1201

---
+++ Programacion3_1598/frmArreglos.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Programacion3_1598
+{
+    partial class frmArreglos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listboxArreglo = new System.Windows.Forms.ListBox();
+            this.btn_button = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listboxArreglo
+            // 
+            this.listboxArreglo.FormattingEnabled = true;
+            this.listboxArreglo.Location = new System.Drawing.Point(124, 162);
+            this.listboxArreglo.Name = "listboxArreglo";
+            this.listboxArreglo.Size = new System.Drawing.Size(188, 199);
+            this.listboxArreglo.TabIndex = 0;
+            // 
+            // btn_button
+            // 
+            this.btn_button.Location = new System.Drawing.Point(145, 26);
+            this.btn_button.Name = "btn_button";
+            this.btn_button.Size = new System.Drawing.Size(182, 86);
+            this.btn_button.TabIndex = 1;
+            this.btn_button.Text = "Ejecutar";
+            this.btn_button.UseVisualStyleBackColor = true;
+            this.btn_button.Click += new System.EventHandler(this.btn_button_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(354, 162);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(164, 199);
+            this.listBox1.TabIndex = 2;
+            // 
+            // frmArreglos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_button);
+            this.Controls.Add(this.listboxArreglo);
+            this.Name = "frmArreglos";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listboxArreglo;
+        private System.Windows.Forms.Button btn_button;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Programacion3_1598/frmArreglos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacion3_1598
+{
+    public partial class frmArreglos : Form
+    {
+        public frmArreglos()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_button_Click(object sender, EventArgs e)
+        {
+            int[] edades = new int[5];
+            edades[0] = 10;
+            edades[1] = 20;
+            edades[2] = 30;
+            edades[3] = 40;
+            edades[4] = 50;
+
+            string[] paises = new string[] { "Honduras", "costa rica", "guatemala", "El salvador", "nicaragua" };
+
+            for(int i=0; i< edades.Length; i++)
+            {
+                listboxArreglo.Items.Add(edades[i]);
+            }
+            foreach(var item in paises)
+            {
+                listBox1.Items.Add(item);
+            }
+
+        }  
+
+    }
+}
